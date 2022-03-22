87d87540314aacb648d80670daa4ba83f6cc327e
Benjamin Carter <benj.james.carter@gmail.com>
2022-03-21 19:57:18 -0700

    completed styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Explorer from './Explorer'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './location.css'
+import './explorer.css'
 
 function App() {
   return (
